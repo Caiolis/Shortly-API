b1b7afa68e21bb3351635e3e9dc4ace5158c0fdc
Caiolis <kaiogesteira@gmail.com>
2023-08-07 17:49:33 -0300

    adjust

--- src/app.js
+++ src/app.js
@@ -11,5 +11,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
